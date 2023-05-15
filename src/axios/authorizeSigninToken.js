fa95aeb885ed70997b0aced73de920bba3adfd32
Olumide Rotimi <rotimiolumide68@gmail.com>
2023-05-15 12:55:10 +0100

    initialzed commit

--- src/axios/authorizeSigninToken.js
+++
@@ -1,8 +0,0 @@
-import { buyNGAxios } from "./axios";
- 
-export const setAuthToken = token => {
-   if (token)
-       buyNGAxios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   else
-       delete buyNGAxios.defaults.headers.common["Authorization"];
-}
--- src/components/product/Product.jsx
+++
@@ -1,9 +0,0 @@
-
-import React,{useEffect} from 'react';
-import { Outlet } from 'react-router-dom';
-
-import "./style.css";
-
-const Product = () => <Outlet/>;
-
-export default Product
